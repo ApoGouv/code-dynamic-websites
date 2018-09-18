31645b8bbd0ad176811bc162ff92c5a362ae0584
ApoGouv <apo.gouv@gmail.com>
2018-09-18 20:14:01 +0300

    Removed trailing whitespace | In dish.php, used number_format instead of money_format

--- 26_Final/Instructor/includes/header.php
+++ 26_Final/Instructor/includes/header.php
@@ -8,21 +8,21 @@
 
 <head>
 	<title><?php echo TITLE; ?></title>
-	<link href="/assets/styles.css" rel="stylesheet">
+	<link href="../../assets/styles.css" rel="stylesheet">
 </head>
 
 <body id="final-example">
-	
+
 	<div class="wrapper">
-	
+
 		<div id="banner">
 			<a href="/" title="Return to Home">
 				<img src="img/banner.png" alt="Franklin's Fine Dining">
-			</a>	
+			</a>
 		</div><!-- banner -->
-		
+
 		<div id="nav">
 			<?php include('includes/nav.php'); ?>
 		</div><!-- nav -->
-		
+
 		<div class="content">
