30e67671a9acb645b2668baad4d5c35fe8f99e4a
ApoGouv <apo.gouv@gmail.com>
2018-09-29 20:29:33 +0300

    menu page and items

--- 26_Final/Student/includes/arrays.php
+++ 26_Final/Student/includes/arrays.php
@@ -3,19 +3,19 @@
     // Nav Menu Items
     $navItems = array(
         array(
-            slug => "index.php",
+            slug  => "index.php",
             title => "Home"
         ),
         array(
-            slug => "team.php",
+            slug  => "team.php",
             title => "Team"
         ),
         array(
-            slug => "menu.php",
+            slug  => "menu.php",
             title => "Menu"
         ),
         array(
-            slug => "contact.php",
+            slug  => "contact.php",
             title => "Contact"
         ),
     );
@@ -39,5 +39,33 @@
             position => "Head Chef",
             bio      => "Carlos is the epitome of the phrase &ldquo;Don't judge a book by it's cover&rdquo; &mdash; You simply cannot find a better chef.",
             img      => "carlos"
+        ),
+    );
+
+    // Menu Items
+    $menuItems = array(
+        "club-sandwich" => array(
+            "title" => "Club Sandwich",
+            "price" => 11,
+            "blurb" => "Bacon ipsum dolor sit amet fatback landjaeger ullamco pariatur. Nostrud laboris et, duis drumstick eiusmod kevin ut aliquip. Filet mignon short ribs tenderloin short loin kielbasa non pork chop aliqua. Jerky shank tri-tip capicola, non andouille tenderloin cupidatat turducken meatball pork beef eu shoulder jowl.",
+            "drink" => "Club Soda"
+        ),
+        "dill-salmon" => array(
+            "title" => "Lemon &amp; Dill Salmon",
+            "price" => 18,
+            "blurb" => "Pork belly tempor ground round qui exercitation, jowl leberkas sed voluptate excepteur jerky. Reprehenderit veniam cow, quis in ribeye andouille eu pastrami eiusmod exercitation dolor.",
+            "drink" => "Fancy Wine"
+        ),
+        "super-salad" => array(
+            "title" => "The Super Salad<sup>&reg;</sup>",
+            "price" => 34,
+            "blurb" => "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.",
+            "drink" => "Jug o' Water"
+        ),
+        "mexican-barbacoa" => array(
+            "title" => "Mexican Barbacoa",
+            "price" => 23,
+            "blurb" => "I love fruitcake danish caramels. Tart danish pastry liquorice chocolate cake fruitcake. Bear claw gingerbread muffin I love apple pie apple pie tiramisu brownie chocolate. Sweet roll cotton candy cupcake gingerbread gummies jelly-o. Muffin I love croissant I love jelly-o brownie jelly beans. Toffee I love pastry.",
+            "drink" => "Beer with a lime"
         ),
     );
